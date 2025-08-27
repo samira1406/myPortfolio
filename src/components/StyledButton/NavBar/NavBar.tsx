086a2284e73cc66bf24cce8471699d95e13289f0
samira1406 <samiraresendef@gmail.com>
2025-08-27 15:12:42 -0300

    feat: creates inits navBar desktop

---
+++ src/components/StyledButton/NavBar/NavBar.tsx
@@ -0,0 +1,23 @@
+import styled from "@emotion/styled";
+import { AppBar, MenuItem, Toolbar } from "@mui/material"
+
+const NavBar = () => {
+
+    const StyledTooBar = styled(Toolbar)(({ theme }) => ({
+        display:"flex",
+        justifyContent:"space-between",
+    }));
+    return (
+        <>
+            <AppBar position="absolute">
+                <StyledTooBar>
+                    <MenuItem> Sobre mim</MenuItem>
+                    <MenuItem> Skills</MenuItem>
+                    <MenuItem> Projetos</MenuItem>
+                </StyledTooBar>
+            </AppBar>
+        </>
+    )
+}
+
+export default NavBar
